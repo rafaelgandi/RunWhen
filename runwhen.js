f877041c1062361c6af66cf02a035c2320c683df
Rafael Gandionco <rafaelgandi@gmail.com>
2022-02-09 11:04:11 +0800

    some syntax updates

--- runwhen.js
+++ runwhen.js
@@ -2,46 +2,45 @@
 	Run When JS
 		- Javascript code dependency checker	
 	See: https://github.com/rafaelgandi/RunWhen
-	LM: 2019-04-18
-	Version: 1.0
+	LM: 2022-02-09
+	Version: 1.1
  */
-var runwhen = (function (self) {
-	// See: https://www.nczonline.net/blog/2012/03/13/its-time-to-start-using-javascript-strict-mode/
-	"use strict";
-	var cachedChecks = {},
+export default (function (self) {
+	let cachedChecks = {},
 		TIMEOUT = 800,
 		check = function (_checks) {
-			var i = _checks.length;
+			let i = _checks.length;
 			while (i--) {
 				if (!! cachedChecks[_checks[i]]) { continue; } // Check cache, try to avoid eval()
 				try {
+                    // DEPRECATED: Dont use runwhen like this. Always pass a function as the first parameter.
 					eval('if(typeof '+_checks[i]+' === \'undefined\'){throw \'e\';}');
 					cachedChecks[_checks[i]] = !0;
 				} 
-				catch(e) { return !1; }	
+				catch (e) { return !1; }	
 			}	
 			return !0;
 		};
 	return function (_checks, _run, _suppressTimeoutError) {	
-		var CHECK_DURATION = 1,
+		let CHECK_DURATION = 1,
 			IS_FUNCTION_PASSED = typeof _checks === 'function';
 		if (! IS_FUNCTION_PASSED) {
 			if (! (_checks instanceof Array)) { _checks = [_checks]; } // Force _checks to be array	
 		}
 		(function loop () {
-			var checking = (! IS_FUNCTION_PASSED) ? check(_checks) : _checks.call(self);
+			let checking = (! IS_FUNCTION_PASSED) ? check(_checks) : _checks.call(self);
 			if (checking) { _run.call(self); }
 			else {
 				// After 800 checks throw an exception //
 				if (CHECK_DURATION > TIMEOUT) { 
 					if (! _suppressTimeoutError) {
-						throw 'RunWhen timeout reached';
+						throw new Error('RunWhen timeout reached');
 					}					
 					return; 
 				}
 				CHECK_DURATION++;
-				requestAnimationFrame(loop);
+				setTimeout(loop, (1000 / 60));
 			}			
 		})();
 	};
-})(self);
+})(window);
